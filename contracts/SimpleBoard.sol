20e64223693e4f8da30e0ff1c800f9e481318366
onokatio <onokatio@gmail.com>
2018-02-24 21:43:12 +0900

    refact

--- contracts/SimpleBoard.sol
+++ contracts/SimpleBoard.sol
@@ -9,11 +9,7 @@ contract SimpleBoard {
         address owner;
         string str;
     }
-    // address[100] owner;
-    // string[100] str;
 		Response[] responses;
-		// address[] ResponseOwner;
-		// string[] ResponseStr;
     
     function postRes(string poststr) payable external {
         // if (responses.length > 100) return;
@@ -22,30 +18,11 @@ contract SimpleBoard {
 				temp.owner = msg.sender;
 				temp.str = poststr;
 
-        // owner[owner.length] = msg.sender;
-        // str[str.length] = poststr;
-        
         responses.push(temp);
-        // ResponseOwner.push(msg.sender);
-        // ResponseStr.push(poststr);
-        // responses[responses.length].owner = msg.sender;
-        // responses[responses.length].str = poststr;
-        // responses[responses.length] = temp;
     }
     function getResponsesAmount() public constant returns(uint){
         return responses.length;
     }
-		/*
-    function getResnum() public constant returns(uint){
-        return owner.length;
-    }
-    function getAllOwner() public constant returns(address[100]){
-        return owner;
-    }
-    function getAllStr() public constant returns(string[100]){
-        return str;
-    }
-		*/
     function getResponseOwner(uint8 resNum) public constant returns (address) {
         return responses[resNum].owner;
     }
