debe9a739894312479b410fff77ebb68178dfbaf
onokatio <onokatio@gmail.com>
2018-02-24 10:12:14 +0900

    addEventListener

--- contracts/SimpleBoard.sol
+++ contracts/SimpleBoard.sol
@@ -9,7 +9,8 @@ contract SimpleBoard {
         address owner;
         string str;
     }
-    Response[] responses;
+    address[100] owner;
+    string[100] str;
     
     function postRes(string str) external {
         if (responses.length > 100) return;
@@ -18,10 +19,14 @@ contract SimpleBoard {
         temp.owner = msg.sender;
         temp.str = str;
         
-        responses.push(temp);
+        //responses.push(temp);
+        responses[responses.length] = temp;
     }
     function getResponsesAmount() public constant returns(uint){
         return responses.length;
+    }
+    function getAllResponses() public constant returns(Response[100]){
+        return responses;
     }
     function getResponseOwner(uint8 resNum) public constant returns (address) {
         return responses[resNum].owner;
